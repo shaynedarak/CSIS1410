28c9b6bc6259339e53684742cbd62fe556d9146b
shaynedarak <70248504+shaynedarak@users.noreply.github.com>
2020-11-02 14:11:28 -0700

    Add files via upload

--- Week 7 Assignment/Main.java
+++ Week 7 Assignment/Main.java
@@ -1,1 +1,41 @@
+import java.io.IOException;
 
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		
+		Employee e1 = new Employee(1);
+		Employee e2 = new Employee(2);
+		Employee e3 = new Employee(3);
+		Employee e4 = new Employee(4);
+		Employee e5 = new Employee(5);
+		Employee e6 = new Employee(6);
+		Employee e7 = new Employee(7);
+		Employee e8 = new Employee(8);
+		Employee e9 = new Employee(9);
+		Employee e10 = new Employee(10);
+		Employee e11 = new Employee(11);
+		Employee e12 = new Employee(12);
+
+		
+
+	
+		e1.printEmployee();
+		e2.printEmployee();
+		e3.printEmployee();
+		e4.printEmployee();
+		e5.printEmployee();
+		e6.printEmployee();
+		e7.printEmployee();
+		e8.printEmployee();
+		e9.printEmployee();
+		e10.printEmployee();
+		e11.printEmployee();
+		e12.printEmployee();
+
+		
+
+	}
+
+}
