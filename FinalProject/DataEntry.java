397d463a9eb03d58130aacb3859388decccfc364
shaynedarak <70248504+shaynedarak@users.noreply.github.com>
2020-12-02 00:00:37 -0700

    Add files via upload

--- FinalProject/DataEntry.java
+++ FinalProject/DataEntry.java
@@ -18,6 +18,7 @@ import javax.swing.SwingConstants;
 import javax.swing.ImageIcon;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
+import javax.swing.JTextArea;
 
 public class DataEntry {
 
@@ -65,37 +66,37 @@ public class DataEntry {
 		frame.getContentPane().setLayout(null);
 
 		JPanel lightGreenbkg = new JPanel();
-		lightGreenbkg.setBackground(new Color(128, 128, 0));
-		lightGreenbkg.setBounds(0, 0, 497, 725);
+		lightGreenbkg.setBackground(new Color(128, 128, 128));
+		lightGreenbkg.setBounds(0, 0, 530, 725);
 		frame.getContentPane().add(lightGreenbkg);
 		lightGreenbkg.setLayout(null);
 
-		JLabel textID = new JLabel("Order #");
+		JLabel textID = new JLabel("Order#");
 		textID.setFont(new Font("Tahoma", Font.BOLD, 14));
 		textID.setForeground(new Color(255, 255, 255));
-		textID.setBounds(31, 112, 74, 14);
+		textID.setBounds(22, 111, 74, 14);
 		lightGreenbkg.add(textID);
 
 		JLabel textName = new JLabel("Name");
 		textName.setFont(new Font("Tahoma", Font.BOLD, 14));
 		textName.setForeground(new Color(255, 255, 255));
-		textName.setBounds(31, 161, 74, 14);
+		textName.setBounds(22, 160, 74, 14);
 		lightGreenbkg.add(textName);
 
 		JLabel textContact = new JLabel("Phone");
 		textContact.setFont(new Font("Tahoma", Font.BOLD, 14));
 		textContact.setForeground(new Color(255, 255, 255));
-		textContact.setBounds(31, 202, 74, 14);
+		textContact.setBounds(22, 201, 74, 14);
 		lightGreenbkg.add(textContact);
 
 		JLabel textCourse = new JLabel("Email");
 		textCourse.setFont(new Font("Tahoma", Font.BOLD, 14));
 		textCourse.setForeground(new Color(255, 255, 255));
-		textCourse.setBounds(31, 252, 74, 14);
+		textCourse.setBounds(22, 251, 74, 14);
 		lightGreenbkg.add(textCourse);
 
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(31, 585, 434, 140);
+		scrollPane.setBounds(80, 593, 427, 121);
 		lightGreenbkg.add(scrollPane);
 
 		table = new JTable();
@@ -115,7 +116,7 @@ public class DataEntry {
 		});
 		table.setBackground(new Color(169, 169, 169));
 		model = new DefaultTableModel();
-		Object[] column = { "Order #", "Name", "Phone", "Email", "Food type", "Beverage type"  };
+		Object[] column = { "Order", "Name", "Phone", "Email", "Food", "Beverage", "Note"  };
 		
 		
 		final Object[] row = new Object[6];//Row numbers
@@ -128,19 +129,12 @@ public class DataEntry {
 		Add.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
-				if (table.getSelectedRow() == -1) {
-					if (table.getRowCount() ==0) {
-						JOptionPane.showMessageDialog(null, "Please fill complete information first to add.");
-					}
-						
-					}
+				if (orderNumber.getText().equals("") || name.getText().equals("") || phone.getText().equals("")
+						|| email.getText().equals("") || food.getSelectedItem().equals("") || beverage.getSelectedItem().equals(""))
 				
-//				if (orderNumber.getText().equals("") || name.getText().equals("") || phone.getText().equals("")
-//						|| email.getText().equals("") || food.getSelectedItem().equals("") || beverage.getSelectedItem().equals(""))
-//				
-//				{
-//					JOptionPane.showMessageDialog(null, "Please fill complete information first to add.");
-//				} 
+				{
+					JOptionPane.showMessageDialog(null, "Please fill complete information first to add.");
+				} 
 				
 				else {
 
@@ -164,7 +158,7 @@ public class DataEntry {
 
 			}
 		});
-		Add.setBounds(94, 302, 135, 29);
+		Add.setBounds(80, 303, 166, 29);
 		lightGreenbkg.add(Add);
 		
 		//Method to Update the Existing.
@@ -192,7 +186,7 @@ public class DataEntry {
 				}
 			}
 		});
-		update.setBounds(330, 302, 135, 29);
+		update.setBounds(347, 303, 160, 29);
 		lightGreenbkg.add(update);
 		
 		//Delete the input order column and row
@@ -210,86 +204,86 @@ public class DataEntry {
 
 			}
 		});
-		delete.setBounds(94, 342, 135, 29);
+		delete.setBounds(80, 343, 166, 29);
 		lightGreenbkg.add(delete);
 		
 			JLabel storeName = new JLabel("Asian Cuisine");
 		storeName.setHorizontalAlignment(SwingConstants.CENTER);
 		storeName.setFont(new Font("Berlin Sans FB", Font.BOLD, 60));
 		storeName.setForeground(new Color(255, 255, 255));
-		storeName.setBounds(72, 11, 403, 74);
+		storeName.setBounds(51, 11, 403, 74);
 		lightGreenbkg.add(storeName);
 
 		orderNumber = new JTextField();
-		orderNumber.setBounds(94, 105, 371, 36);
+		orderNumber.setBounds(80, 105, 427, 36);
 		lightGreenbkg.add(orderNumber);
 		orderNumber.setColumns(10);
 
 		name = new JTextField();
 		name.setColumns(10);
-		name.setBounds(94, 152, 371, 36);
+		name.setBounds(80, 152, 427, 36);
 		lightGreenbkg.add(name);
 
 		phone = new JTextField();
 		phone.setColumns(10);
-		phone.setBounds(94, 199, 371, 36);
+		phone.setBounds(80, 199, 427, 36);
 		lightGreenbkg.add(phone);
 
 		email = new JTextField();
 		email.setColumns(10);
-		email.setBounds(94, 246, 371, 36);
+		email.setBounds(80, 246, 427, 36);
 		lightGreenbkg.add(email);
 
 		JComboBox food = new JComboBox();
 		food.setFont(new Font("Tahoma", Font.BOLD, 14));
-		food.setModel(new DefaultComboBoxModel(new String[] {"Pho", "Egg roll", "Fried rice", "Fried shrimps", "Fried chickens", "Beff Jerky (Asian Style)", "Sweet rice", "Papaya Salad", "Sweet and sour soup (Seafood, Chicken meat)"}));
+		food.setModel(new DefaultComboBoxModel(new String[] {"1. Pho", "2. Egg roll", "3. Fried rice", "4. Fried shrimps", "5. Fried chickens"}));
 		food.setSelectedIndex(-1);
-		food.setBounds(94, 422, 184, 36);
+		food.setBounds(80, 422, 166, 36);
 		lightGreenbkg.add(food);
 
 		JLabel textFood = new JLabel("Food");
 		textFood.setForeground(Color.WHITE);
 		textFood.setFont(new Font("Tahoma", Font.BOLD, 16));
-		textFood.setBounds(94, 397, 114, 14);
+		textFood.setBounds(80, 397, 114, 14);
 		lightGreenbkg.add(textFood);
 
 		JComboBox beverage = new JComboBox();
 		beverage.setFont(new Font("Tahoma", Font.BOLD, 14));
-		beverage.setModel(new DefaultComboBoxModel(new String[] {"Water", "Thai Tea", "Tea", "Pepsi", "Boba"}));
+		beverage.setModel(new DefaultComboBoxModel(new String[] {"1. Water", "2. Thai Tea", "3. Tea", "4. Pepsi", "5. Boba"}));
 		beverage.setSelectedIndex(-1);
-		beverage.setBounds(289, 422, 176, 36);
+		beverage.setBounds(347, 422, 160, 36);
 		lightGreenbkg.add(beverage);
 
 		JLabel textBeverage = new JLabel("Beverage");
 		textBeverage.setForeground(Color.WHITE);
 		textBeverage.setFont(new Font("Tahoma", Font.BOLD, 16));
-		textBeverage.setBounds(289, 397, 114, 14);
+		textBeverage.setBounds(347, 397, 114, 14);
 		lightGreenbkg.add(textBeverage);
 
 		JPanel greyBackground = new JPanel();
 		greyBackground.setBackground(new Color(105, 105, 105));
-		greyBackground.setBounds(0, 0, 1086, 725);
+		greyBackground.setBounds(0, 0, 1076, 725);
 		frame.getContentPane().add(greyBackground);
 		greyBackground.setLayout(null);
 
 		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("C:\\Users\\chane\\eclipse-workspace\\Final\\images\\pho.png"));
+		lblNewLabel.setIcon(new ImageIcon("images\\pho.png"));
 		lblNewLabel.setVerticalAlignment(SwingConstants.TOP);
-		lblNewLabel.setBounds(282, 203, 529, 383);
+		lblNewLabel.setBounds(312, 213, 423, 385);
 		greyBackground.add(lblNewLabel);
 
 		JPanel panel = new JPanel();
-		panel.setBackground(new Color(128, 128, 0));
-		panel.setBounds(0, 725, 1086, 45);
+		panel.setBackground(new Color(255, 255, 255));
+		panel.setBounds(0, 725, 1076, 35);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 
 		JLabel lblNewLabel_2 = new JLabel("Version: 1.0");
-		lblNewLabel_2.setBounds(33, 11, 81, 14);
+		lblNewLabel_2.setBounds(74, 11, 81, 14);
 		lblNewLabel_2.setBackground(new Color(105, 105, 105));
 		panel.add(lblNewLabel_2);
-		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setForeground(new Color(105, 105, 105));
 		
 		//clear out the form
 		JButton clear = new JButton("Clear");
@@ -304,8 +298,24 @@ public class DataEntry {
 				beverage.setSelectedIndex(-1);
 			}
 		});
-		clear.setBounds(330, 342, 135, 29);
+		clear.setBounds(347, 343, 160, 29);
 		lightGreenbkg.add(clear);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(80, 503, 427, 59);
+		lightGreenbkg.add(textArea);
+		
+		JLabel lblNote = new JLabel("Note");
+		lblNote.setForeground(Color.WHITE);
+		lblNote.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblNote.setBounds(80, 479, 74, 14);
+		lightGreenbkg.add(lblNote);
+		
+		JLabel lblRecord = new JLabel("Record");
+		lblRecord.setForeground(Color.WHITE);
+		lblRecord.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblRecord.setBounds(80, 573, 74, 14);
+		lightGreenbkg.add(lblRecord);
 
 
 	}
