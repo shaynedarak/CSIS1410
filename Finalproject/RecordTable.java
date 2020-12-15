50f1bb3f4892941c9e7c3b03982425d62224c5a4
shaynedarak <70248504+shaynedarak@users.noreply.github.com>
2020-12-15 03:13:52 -0700

    Update RecordTable.java

--- Finalproject/RecordTable.java
+++ Finalproject/RecordTable.java
@@ -1,26 +1,46 @@
 import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JLabel;
-import java.awt.Font;
-import javax.swing.UIManager;
-import java.awt.SystemColor;
-import javax.swing.JTable;
 import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.UIManager;
+import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Toolkit;
 
 public class RecordTable extends JFrame {
 
 	private JPanel contentPane;
-	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RecordTable frame = new RecordTable();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 	/**
 	 * Create the frame.
 	 */
 	public RecordTable() {
+		setIconImage(Toolkit.getDefaultToolkit().getImage("images\\blackteapot.png"));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 646, 430);
 		contentPane = new JPanel();
@@ -29,22 +49,29 @@ public class RecordTable extends JFrame {
 		contentPane.setLayout(null);
 		
 		JPanel panel = new JPanel();
-		panel.setBackground(Color.DARK_GRAY);
+		panel.setBackground(new Color(105, 105, 105));
 		panel.setBounds(0, 0, 630, 391);
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
 		JLabel asian = new JLabel("Asian Cuisine");
-		asian.setBounds(20, 15, 148, 37);
+		asian.setBackground(new Color(255, 255, 255));
+		asian.setBounds(10, 11, 189, 28);
 		panel.add(asian);
-		asian.setForeground(UIManager.getColor("Button.background"));
-		asian.setFont(new Font("Tahoma", Font.BOLD, 20));
+		asian.setForeground(new Color(255, 255, 255));
+		asian.setFont(new Font("Berlin Sans FB", Font.BOLD, 25));
 		
-		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(20, 63, 588, 303);
-		panel.add(scrollPane);
+		JScrollPane EntryHistory = new JScrollPane();
+		EntryHistory.setBounds(10, 50, 610, 330);
+		panel.add(EntryHistory);
 		
-		table = new JTable();
-		scrollPane.setViewportView(table);
+		JTable table = new JTable();
+		EntryHistory.setViewportView(table);
+		
 	}
+	public void setVisible() {
+		this.setVisible(true);
+	}
+	
+
 }
